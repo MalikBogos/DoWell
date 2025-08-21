9b6163680218facc2247e0460e524934152a4002
MalikBogos <malikjandarov@gmail.com>
2025-08-21 21:35:43 +0200

    Added basic MenuBar layout with new View folder for View files

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,19 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DoWell
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        
+
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
         }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
     }
 }
